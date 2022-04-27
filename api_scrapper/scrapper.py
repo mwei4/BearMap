53ad7f3bc062f8fcf74f3a75176381984585b971
Michael <43123073+mwei4@users.noreply.github.com>
2022-04-26 20:59:58 -0400

    added some scraping functionality

--- api_scrapper/scrapper.py
+++ api_scrapper/scrapper.py
@@ -1,14 +1,24 @@
 import requests
 import json
-response = requests.get(
-    "https://classes.cornell.edu/api/2.0/search/classes.json?roster=WI22&subject=ECON")
+
+url = "https://classes.cornell.edu/api/2.0/search/classes.json?roster=WI22&subject=ECON"
+response = requests.get(url)
 data = json.loads(response.text)["data"]["classes"]
-title = "titleLong"
-for i in range(len(data)):
-    print(data[i]["description"])
-# print(data[0]["subject"])
-
-quote = '成功を収める人とは人が投げてきたレンガでしっかりした基盤を築くことができる人のことである。'
-
-with open('quotes.txt', 'w', encoding='utf-8') as f:
-    f.write(quote)
+file_name = url[url.find("subject=")+8:] + "_" + \
+    url[url.find("roster=")+7:url.find("roster=") + 11] + ".txt"
+with open(file_name, 'w', encoding='utf-8') as f:
+    for i in range(len(data)):
+        f.write(data[i]["titleLong"] + "\n")
+        f.write(data[i]["description"] + "\n")
+        # if data[i]["enrollGroups"]["classSections"]["notes"]["unitsMinimum"] != data[i]["enrollGroups"]["classSections"]["notes"]["unitsMaximum"]:
+        #     f.write("Credits: " + data[i]["enrollGroups"]["classSections"]["notes"]["unitsMinimum"] +
+        #           "-" + data[i]["enrollGroups"]["classSections"]["notes"]["unitsMaximum"])
+        # else:
+        #     f.write("Credits: " + data[i]["enrollGroups"]
+        #           ["classSections"]["unitsMinimum"] + "\n")
+        f.write("Semesters Offered: " + data[i]["catalogWhenOffered"] + "\n")
+        if data[i]["catalogPrereqCoreq"] == "":
+            f.write("Pre/Corequisites: None")
+        else:
+            f.write("Pre/Corequisites: " + data[i]["catalogPrereqCoreq"])
+        f.write("\n\n")
