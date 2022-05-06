c01fc4d3c8ec9f67e27dbaa3e7eb8cf2b0939173
Michael <mw756@cornell.edu>
2022-05-06 13:54:45 -0400

    added prereq list

--- api_scrapper/scrapper/prereq_parse.py
+++ api_scrapper/scrapper/prereq_parse.py
@@ -30,7 +30,7 @@ def prereq_parse(input):
 
 # [file_name] is parsed from [semester] and [subject] in form [subject_semester]
 file_name = url[url.find("subject=")+8:] + "_" + \
-    url[url.find("roster=")+7:url.find("roster=") + 11] + ".json"
+    url[url.find("roster=")+7:url.find("roster=") + 11] + "_PreReq.json"
 
 for i in range(len(data)):
     # [titleLong] is course name, [subject] is subject, [catalogNbr] is
@@ -38,19 +38,6 @@ for i in range(len(data)):
     current_class = {}
     current_class["class"] = data[i]['subject'] + \
         " " + data[i]['catalogNbr']
-    current_class["name"] = data[i]["titleLong"]
-    # [description] is course description
-    current_class["description"] = data[i]["description"].replace(
-        "\u00a0", " ").strip()
-    # [if-else] clause for course credit count
-    # [if] clause checks if min and max credit units are different
-    if data[i]["enrollGroups"][0]["unitsMinimum"] != data[i]["enrollGroups"][0]["unitsMaximum"]:
-        current_class["credits"] = [data[i]["enrollGroups"][0]
-                                    ["unitsMinimum"], data[i]["enrollGroups"][0]["unitsMaximum"]]
-    else:
-        current_class["credits"] = data[i]["enrollGroups"][0]["unitsMinimum"]
-    # [catalogWhenOffered] is the semesters in which course is offered
-    current_class["offered"] = data[i]["catalogWhenOffered"]
     # [catalogPrereqCoreq] lists the pre- and co-reqs of a course
     pre_coreqs = data[i]["catalogPrereqCoreq"].replace(
         "\u00a0", " ")
@@ -65,22 +52,7 @@ for i in range(len(data)):
     prereq_lst = list(filter(lambda x: x.startswith(
         "Prerequisite") or x.startswith("Corequisite"), prereq_lst))
     prereq_lst = prereq_parse(" ".join(prereq_lst))
-    current_class["pre_coreq list"] = prereq_lst
-
-    # [firstName] + [lastName] lists all instructors by order of [firstName lastName]
-    instructors = data[i]["enrollGroups"][0]["classSections"][0]["meetings"][0]["instructors"]
-    instructor_names = []
-    for j in range(len(instructors)):
-        instructor_names.append(instructors[j]["firstName"] +
-                                " " + instructors[j]["lastName"])
-    current_class["instructors"] = instructor_names
-    # scrape crosslisted courses
-    crosslisted_courses = data[i]["enrollGroups"][0]["simpleCombinations"]
-    crosslisted = []
-    for j in range(len(crosslisted_courses)):
-        crosslisted.append(crosslisted_courses[j]["subject"] +
-                           " " + crosslisted_courses[j]["catalogNbr"])
-    current_class["crosslisted"] = crosslisted
+    current_class["pre_coreq lst"] = prereq_lst
     all_classes[current_class["class"]] = current_class
 with open(file_name, "w") as f:
     f.write(json.dumps(all_classes))
