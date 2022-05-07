78c340379c05e13041a3e627fec268422aae3299
Josh De Leeuw <joshuamdeleeuw@gmail.com>
2022-05-06 21:07:12 -0400

    Display unlocked classes as well

--- components/layout/ClassInfo.js
+++ components/layout/ClassInfo.js
@@ -1,20 +1,29 @@
 import React, {ReactNode} from 'react'
 import json_data from "../../api_scrapper/scrapper/CS_SimplePre.json"
+import post_data from "../../api_scrapper/scrapper/CS_SimplePosts.json"
 
 let map = new Map()
 
 const ClassInfo = ({title}) => {
-    let classes = json_data[title]
+    let classes = "Prereqs: " + json_data[title]
+    let after = "Unlocked: " + post_data[title]
+
     if (typeof(classes) == 'undefined') {
         classes = "Not a valid class for this semester!"
     }
     else if (classes.length == 0) {
         classes = "No Prereqs!"
     }
+    if (typeof(after) == 'undefined') {
+        after = ""
+    }
+    else if (after.length == 0) {
+        after = "Nothing Unlocked"
+    }
 
     return (
         
-        <div>{classes}</div>
+        <><div>{classes}</div><br></br><div>{after}</div></>
     )
 }
 
