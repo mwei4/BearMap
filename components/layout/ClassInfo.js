896cd9e21b226b6274978eef9174ccea9f6a641d
Michael <mw756@cornell.edu>
2022-05-13 21:51:42 -0400

    refactoring

--- components/layout/ClassInfo.js
+++ components/layout/ClassInfo.js
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react'
+import React, { ReactNode } from 'react'
 import json_data from "../../api_scrapper/scrapper/CS_SimplePre.json"
 import post_data from "../../api_scrapper/scrapper/CS_SimplePosts.json"
 import original_data from "../../api_scrapper/scrapper/CS_SP22.json"
@@ -6,7 +6,7 @@ import { getEventListeners } from 'stream'
 
 let map = new Map()
 
-const ClassInfo = ({title}) => {
+const ClassInfo = ({ title }) => {
 
     let pre = json_data[title]
     let post = post_data[title]
@@ -18,20 +18,19 @@ const ClassInfo = ({title}) => {
     let instructors = []
     let description = ""
 
-    if (typeof(pre) != "undefined") {
-        //functional programming
+    if (typeof (pre) != "undefined") {
         pre = pre.map(x => " " + x)
         classes = "Prereqs: " + pre
         instructors = original_data[title].instructors
         instructors = "Instructors: " + instructors.map(x => " " + x)
-        name = original_data[title].class + " (" + original_data[title].name + "), Credits: " + 
-        original_data[title].credits
+        name = original_data[title].class + " (" + original_data[title].name + "), Credits: " +
+            original_data[title].credits
         description = original_data[title].description
         post = post.map(y => " " + y)
         after = "Unlocked: " + post
     }
     return (
-        
+
         <><><><><><br /><div>{name}</div><br /><div>{classes}</div>
             <br /></><div>{after}</div><br></br></><div>{instructors}</div></></><br></br><div>{description}</div></>
     )
